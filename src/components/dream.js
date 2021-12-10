d1ec79b15fd27c31f20ec10d3090e216232ee83a
Anthony Gonzales <anthonygonzales79072@gmail.com>
2021-12-10 15:30:30 -0600

    added clouds

--- src/components/dream.js
+++ src/components/dream.js
@@ -7,13 +7,35 @@ const Dream = ({dream}) => {
         <h1>{dream.title}</h1>
         <h2>{dream.date}</h2>
         <img src={dream.image} alt="dreampic"/> */}
+        {/* Cloud affect source = https://codepen.io/Mark_Bowley/pen/xEbuI */}
+          <div id="background-wrap">
+         <div class="x1">
+            <div class="cloud"></div>
+          </div>
+
+            <div class="x2">
+            <div class="cloud"></div>
+            </div>
+
+             <div class="x3">
+          <div class="cloud"></div>
+            </div>
+
+            <div class="x4">
+            <div class="cloud"></div>
+              </div>
+
+           <div class="x5">
+            <div class="cloud"></div>
+           </div>
+           </div>
         <div class="card">
          <img src={dream.image} class="card-img-top" alt="cardimage"/>
           <div class="card-body">
            <h5 class="card-title">{dream.title}</h5>
            <p class="card-text">{dream.description}</p>
            <Link to={`/dream/${dream.id}`}>
-             <button>View Log</button>
+             <button class="view">View Log</button>
            </Link>
           </div>
         </div>
--- src/pages/OneDream.js
+++ src/pages/OneDream.js
@@ -1,19 +1,19 @@
-import { Link, useParams } from "react-router-dom";
+import {  useParams } from "react-router-dom";
 
 const OneDream = ({dreams, edit, deleteDream}) => {
     const params = useParams()
     const id = parseInt(params.id)
     const dream = dreams.find((dream) => dream.id === id)
     return (<div>
-        <Link to="/">
+        {/* <Link to="/">
         <button type="button" class="btn btn-info">Home</button>
-        </Link>
-        <button type="button" class="btn btn-primary" onClick={(event) => edit(dream)}>Edit Dream</button>
-        <button type="button" class="btn btn-danger" onClick={(event) => deleteDream(dream)}>Delete Dream</button>
+        </Link> */}
         <h1>{dream?.title}</h1>
+        <h2>{dream?.date}</h2>
         <h2>{dream?.description}</h2>
         <img src={dream?.image} alt="singledream"/>
-        <h2>{dream?.date}</h2>
+        <button type="button" class="btn btn-primary" onClick={(event) => edit(dream)}>Edit Dream</button>
+        <button type="button" class="btn btn-danger" onClick={(event) => deleteDream(dream)}>Delete Dream</button>
     </div>)
 }
 export default OneDream
